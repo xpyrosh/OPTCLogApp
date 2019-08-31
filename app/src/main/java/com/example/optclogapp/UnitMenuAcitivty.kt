5dff8565f3a671143f3f40986cb791697bfdfc14
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-08-31 15:16:10 -0400

    Hover Event made no sense, gave up on trying to click animate my imageviews (for now); added click events for all the posters and moving onto the next step

--- app/src/main/java/com/example/optclogapp/UnitMenuAcitivty.kt
+++ app/src/main/java/com/example/optclogapp/UnitMenuAcitivty.kt
@@ -1,17 +1,22 @@
 package com.example.optclogapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlin.math.log
 
 class UnitMenuAcitivty : AppCompatActivity(){
     lateinit var legendImage : ImageView
     lateinit var lrrImage : ImageView
     lateinit var rrImage : ImageView
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_unit_menu)
@@ -21,14 +26,24 @@ class UnitMenuAcitivty : AppCompatActivity(){
         rrImage=findViewById(R.id.rrImage)
 
         legendImage.setOnClickListener{
+            Log.d("OPTCLog", "Legend Click Event")
             Toast.makeText(this, "Legend Units", Toast.LENGTH_SHORT).show()
-            legendImage.setImageResource(R.drawable.goldposter)
+            legendImage.setImageResource(R.drawable.legendposter2)
         }
 
-        legendImage.setOnHoverListener{ view: View, motionEvent: MotionEvent ->
-            legendImage.setImageResource(R.drawable.goldposter)
-            true
+        lrrImage.setOnClickListener{
+            Log.d("OPTCLog", "LRR Click Event")
+            Snackbar.make(lrrImage, "LRR Units", Snackbar.LENGTH_SHORT).show()
+            lrrImage.setImageResource(R.drawable.goldposter2)
         }
+
+        rrImage.setOnClickListener{
+            Log.d("OPTCLog", "RR Click Event")
+            rrImage.setImageResource(R.drawable.goldposter2)
+        }
+
+        // legendImage.setOnTouchListener { view , motionEvent -> }
+        // COME BACK TO THIS AFTER, THIS POSTER WILL FLUTTER ON CLICK AND RELEASE
 
     }
 }
